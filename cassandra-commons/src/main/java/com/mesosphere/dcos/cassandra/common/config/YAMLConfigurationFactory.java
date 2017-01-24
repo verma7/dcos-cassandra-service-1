fd675901ccd43c8caf91ea0cbccf14de29bb675f
Abhishek Verma <verma@uber.com>
2017-01-23 17:10:08 -0800

    Do not fail if we encounter unknown properties so that we can safely roll back the scheduler configuration which may not have the new field values.

--- cassandra-commons/src/main/java/com/mesosphere/dcos/cassandra/common/config/YAMLConfigurationFactory.java
+++ cassandra-commons/src/main/java/com/mesosphere/dcos/cassandra/common/config/YAMLConfigurationFactory.java
@@ -1,5 +1,6 @@
 package com.mesosphere.dcos.cassandra.common.config;
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.guava.GuavaModule;
 import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
@@ -22,7 +23,10 @@ public class YAMLConfigurationFactory implements ConfigurationFactory<Configurat
             final ObjectMapper mapper = Jackson.newObjectMapper()
                     .registerModule(new GuavaModule())
                     .registerModule(new JavaTimeModule())
-                    .registerModule(new Jdk8Module());
+                    .registerModule(new Jdk8Module())
+                    // Do not fail if we encounter unknown properties so that we can safely roll back
+                    // the scheduler configuration which may not have the new field values.
+                    .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
             return (Configuration) mapper.readValue(bytes, typeParameterClass);
         } catch (Exception e) {
             throw new ConfigStoreException(e);
